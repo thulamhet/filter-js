e6f1b609d5dbc7b6d225db3d082820c599ceec74
thulamhet <61628978+thulamhet@users.noreply.github.com>
2021-06-19 15:50:48 +0700

    hehe

---
+++ getDoc.js
@@ -0,0 +1,48 @@
+
+let input = `bat dau tu <h4>Cau1`
+input = input.replace(/(^[ \t]*\n)/gm, "")
+for(let i = 0; i < input.length; i++) {
+    let value;
+    if(input.slice(i, i + 3) === 'Câu') {
+        let j = i;
+        for (j = i; j < input.length; j++) {
+            if(input.slice(j, j + 4) === '</p>') {
+                console.log(input.slice(i, i + 6) );
+                console.log(input.slice(i + 15, j ));
+                for (let k = j; k < input.length; k++) {
+                    if(input.slice(k, k + 5) === 'value') {
+                        value = input[k + 7];
+                        for (let m = k; m < input.length; m++) {
+                            let answer;
+                            switch(value) {
+                                case '1': 
+                                    answer = 'A';
+                                    break;
+                                case '2': 
+                                    answer = 'B';
+                                    break;
+                                case '3': 
+                                    answer = 'C';
+                                    break;
+                                default: 
+                                    answer = 'D'
+                                    break;
+                            }
+                            if(input.slice(m, m + 2) === answer + '.') {
+                                for (let n = m; n < input.length; n++) {
+                                    if(input.slice(n, n + 3) === '</l') {
+                                        console.log(input.slice(m, n))
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                break;
+            };
+        }
+    }
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < input.length; i++) {
+//     if(input.slice(i, i+3) == 'Câu') {
+//         let j = i;
+//         for (j = i; j < input.length; j++) {
+//             if(input.slice(j, j+4) = '</p>') break;
+//         }
+//         console.log(i + 10, i + 10 + j);
+//         console.log(input.slice(i, i + 20))
+//     }
+// }
